b29267d5962609c40cfbf1be992ba2caf77302be
Ladislav Thon <lthon@redhat.com>
2021-03-23 15:28:10 +0100

    add a tiny AMQP project

---
+++ amqp/src/main/java/cz/ladicek/quarkus/tiny/amqp/Producer.java
@@ -0,0 +1,18 @@
+package cz.ladicek.quarkus.tiny.amqp;
+
+import io.smallrye.mutiny.Multi;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.time.Duration;
+
+@ApplicationScoped
+public class Producer {
+    @Outgoing("ticks-producer")
+    public Multi<Message<String>> produce() {
+        return Multi.createFrom()
+                .ticks().startingAfter(Duration.ofMillis(2000)).every(Duration.ofMillis(1000))
+                .map(it -> Message.of("" + it));
+    }
+}
